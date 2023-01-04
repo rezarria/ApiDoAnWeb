780c2ac8a7f407a177fcd67cea5e344589101bff
rezarria <votanamcb@outlook.com>
2023-01-04 07:02:00 +0700

    up

---
+++ Services/CauNoiClient.cs
@@ -0,0 +1,24 @@
+﻿using Api.Contexts;
+
+namespace Api.Services;
+
+public class CauNoiClientService
+{
+	private readonly IServiceScope _serviceScope;
+	private readonly CauHinhDbContext _hinhDbContext;
+	private readonly ILogger _logger;
+
+
+	public CauNoiClientService(IServiceProvider serviceProvider)
+	{
+		_serviceScope = serviceProvider.CreateScope();
+		_hinhDbContext = _serviceScope.ServiceProvider.GetRequiredService<CauHinhDbContext>();
+		_logger = _serviceScope.ServiceProvider.GetRequiredService<ILogger<CauNoiClientService>>();
+	}
+
+	public Task KiemTra(CancellationToken cancellationToken)
+	{
+		_logger.LogInformation("✔");
+		return Task.CompletedTask;
+	}
+}
